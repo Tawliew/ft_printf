f6160286aba9992225c5e27301614bdd9e106075
Luiz Fernando <lfluiz.lf@gmail.com>
2021-08-10 23:38:57 -0300

    add parameter %s

--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,14 @@
 /*   By: luizfern <lfluiz.lf@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:06:41 by luizfern          #+#    #+#             */
-/*   Updated: 2021/08/10 00:37:29 by luizfern         ###   ########.fr       */
+/*   Updated: 2021/08/10 23:35:08 by luizfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // testes inicias com ft_printf
-//
-//
-//
 
+# include "libftprintf.h"
+# include <stdio.h>
 char	*ft_strchr(const char *s, int c)
 {
 	char	*str;
@@ -30,11 +29,23 @@ char	*ft_strchr(const char *s, int c)
 		return (str);
 	return (0);
 }
+int	_verifier_percent_s(char *string)
+{
+	int counter;
 
-# include "libftprintf.h"
+	counter = 0;
+	while (string[counter])
+		write(1, &string[counter++], 1);
+	return (counter);
+}
 int	_verifier_percent_c(int c)
 {
-	return (write(1, &c, 1));
+	int counter;
+	
+	counter = write(1, &c, 1);
+
+	printf("\n\n counter inside verifier %d", counter);
+	return (counter);
 }
 
 int	_global_verifier(int format, va_list c)
@@ -43,8 +54,12 @@ int	_global_verifier(int format, va_list c)
 
 	if (format == 'c')
 		contador = _verifier_percent_c(va_arg(c, int));
+
+	else if (format == 's')
+		contador =  _verifier_percent_s(va_arg(c, char *));
 	else
 		contador = 0;
+	printf("\n\n contador: %d\n\n", contador);
 	return (contador);
 }
 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: luizfern <lfluiz.lf@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:10:45 by luizfern          #+#    #+#             */
-/*   Updated: 2021/08/10 00:39:44 by luizfern         ###   ########.fr       */
+/*   Updated: 2021/08/10 23:36:31 by luizfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,34 @@ int main (int argc, char **argv)
 {
 	int return_printf;
 	char a = 'a';
-
+	char b[] = "teste s";
 	if (argc < 1)
 		return (0);
 	
 	if (argc == 2)
 	{
-		return_printf = ft_printf("ronaldinho gaucho %c %c", a, a);
+		printf("\n\n ---------------format c testes 1------------------ \n\n");
+		return_printf = ft_printf("o caracter e: %c", a);
 		printf("\n\n");
 		printf("Retorno do ft_printf: %d", return_printf);
 		printf("\n\n");
-		return_printf = printf("ronaldinho gaucho %c %c", a, a);
+		return_printf = printf("O caracter e: %c", a);
+		printf("\n\n");
+		printf("Retorno da printf: %d", return_printf);
+		printf("\n\n ---------------format c testes 1------------------ \n\n");
+		return_printf = ft_printf("o char %c e igual ao char %c", a, a);
+		printf("\n\n");
+		printf("Retorno do ft_printf: %d", return_printf);
+		printf("\n\n");
+		return_printf = printf("o char %c e igual ao char %c", a, a);
+		printf("\n\n");
+		printf("Retorno da printf: %d", return_printf);
+		printf("\n\n ---------------format s testes------------------ \n\n");
+		return_printf = ft_printf("A string e: %s", b);
+		printf("\n\n");
+		printf("Retorno da ft_printf: %d", return_printf);
+		printf("\n\n");
+		return_printf = printf("A string e: %s", b);
 		printf("\n\n");
 		printf("Retorno da printf: %d", return_printf);
 	}
